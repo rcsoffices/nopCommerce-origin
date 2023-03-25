0204a3374ef9f16852346d8dd0ece4bc723597a5
RACASSIN Mathieu <racassin.mathieu@gmail.com>
2023-03-25 11:49:16 +0100

    not finish

---
+++ src/Plugins/Nop.Plugin.Cart.Form/CartFormPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Plugin.Cart.Form.Component;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Cart.Form
+{
+    public class CartFormPlugin : BasePlugin, IWidgetPlugin
+    {
+        public CartFormPlugin()
+        {
+        }
+
+        public bool HideInWidgetList => false;
+
+        public Type GetWidgetViewComponent(string widgetZone)
+        {
+            return typeof(CartFormPublicViewComponent);
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { "home_page_before_categories" });
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Cart.Form/Component/CartFormPublicViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Cart.Form.Component
+{
+    public class CartFormPublicViewComponent : NopViewComponent
+    {
+        public CartFormPublicViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        {
+            return View("~/Plugins/Cart.Form/Views/PublicForm.cshtml");
+        }
+    }
+}
