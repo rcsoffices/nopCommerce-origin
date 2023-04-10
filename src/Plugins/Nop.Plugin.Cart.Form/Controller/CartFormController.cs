c4d99ea25eb352427b5af2393b0dbadbd4695948
RACASSIN Mathieu <racassin.mathieu@gmail.com>
2023-04-10 09:47:26 +0200

    add theme

---
+++ src/Plugins/Nop.Plugin.Cart.Form/Controller/CartFormController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Cart.Form.Controller
+{
+    public class CartFormController : BasePluginController
+    {
+        public CartFormController()
+        {
+            
+        }
+
+    }
+}
