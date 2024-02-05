5afa728e06378a6f9093a35e845d7c7468cfb2f6
RACASSIN Mathieu <racassin.mathieu@gmail.com>
2024-02-05 11:54:35 +0100

    chore : update style

---
+++ src/Plugins/Nop.Plugin.Product.Stepper/Controllers/FrontFormController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.EMMA;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Product.Stepper.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class FrontFormController : BasePluginController
+    {
+        public async Task<IActionResult> GetProductForm(int productId)
+        {
+            return View("~/Plugins/Nop.Plugin.Product.Stepper/Views/PublicInfo.cshtml");
+        }
+    }
+
+}
